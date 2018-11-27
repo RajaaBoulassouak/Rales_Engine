8b9725e38f7878d893975a430cfd2728c5550edd
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-27 16:42:48 -0700

    sets up all relationships for all categories with tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Customer, type: :model do
     it {should validate_presence_of :first_name}
     it {should validate_presence_of :last_name}
   end  
+  
+  describe 'Relationships' do 
+    it {should have_many :invoices}
+    it {should have_many :merchants}
+    it {should have_many :transactions}
+    it {should have_many :invoice_items}
+  end 
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe InvoiceItem, type: :model do
     it {should validate_presence_of :quantity}
     it {should validate_presence_of :unit_price}
   end 
+  
+  describe 'Relationships' do 
+    it {should belong_to :invoice}
+    it {should belong_to :item}
+  end
 end

d6c9d82f01f14898e74190b76ea8063caf7acd5f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-30 16:58:25 -0700

    adds serializers for total revenue and revenue and modifies revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
-
+  
+  def show 
+    if params[:date]
+      revenue = Merchant.single_merchant_revenue_by_date(params)
+      render json: RevenueSerializer.new(Revenue.new(revenue))
+    else
+      render json: Revenue::MerchantSerializer.new(Merchant.single_merchant_revenue(params[:id]))
+    end
+  end
+  
   def index
-    revenue = Merchant.revenue_by_date(params[:date])
-    render json: {total_revenue: revenue.to_json}
+    total_revenue = Merchant.revenue_by_date(params[:date])
+    render json: TotalRevenueSerializer.new(TotalRevenue.new(total_revenue))
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,11 +10,11 @@ class Merchant < ApplicationRecord
   
   def self.most_revenue(quantity)
     unscoped
-    .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
-    .joins(items: :invoice_items)
-    .group(:id)
-    .order("total_revenue DESC")
-    .limit(quantity)
+      .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+      .joins(items: :invoice_items)
+      .group(:id)
+      .order("total_revenue DESC")
+      .limit(quantity)
   end
   
   def self.most_revenue(quantity)
@@ -42,6 +42,24 @@ class Merchant < ApplicationRecord
       .joins(invoices: [:invoice_items, :transactions])
       .merge(Transaction.unscoped.successful)
       .where(invoices: {created_at: date.to_date.beginning_of_day..date.to_date.end_of_day} )
+      .sum("quantity * unit_price")
+  end
+  
+  def self.single_merchant_revenue(id)
+    unscoped
+      .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.unscoped.successful)
+      .where(id: id)
+      .group(:id)
+  end
+  
+  def self.single_merchant_revenue_by_date(params)
+    unscoped
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.unscoped.successful)
+      .where(invoices: {created_at: params[:date].to_date.beginning_of_day..params[:date].to_date.end_of_day} )
+      .where(id: params[:id])
       .sum("quantity * unit_price")
   end
 end

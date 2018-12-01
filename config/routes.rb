bba4513c6a0c46652c1cb1d6167f7cd27530ee9e
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-01 16:43:16 -0700

    adds routes, controller, and show action for random find for each category

--- config/routes.rb
+++ config/routes.rb
@@ -8,31 +8,38 @@ Rails.application.routes.draw do
         get '/:id/revenue'  => 'revenue#show'
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show' 
       end 
       
       namespace :customers do 
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show'
       end 
       
       namespace :items do 
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show'
       end 
       
       namespace :invoices do 
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show'
+        get '/random'       => 'random#show'
       end 
       
       namespace :invoice_items do 
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show'
       end 
       
       namespace :transactions do 
         get '/find'         => 'find#show'
         get '/find_all'     => 'find#index'
+        get '/random'       => 'random#show'
       end 
        
       resources :merchants, only: [:index, :show]

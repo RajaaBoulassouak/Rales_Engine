c765b6e03ee28f1e0b26fbf2e7ff1c367aecf0c1
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-02 19:50:45 -0700

    adds route, controller, show action and AR model method to get favorite customer

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,31 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       namespace :merchants do 
-        get '/most_revenue' => 'most_revenue#index'
-        get '/most_items'   => 'most_items#index'
-        get '/revenue'      => 'revenue#index'
+        get '/most_revenue'          => 'most_revenue#index'
+        get '/most_items'            => 'most_items#index'
+        get '/revenue'               => 'revenue#index'
         get '/:id/favorite_customer' => 'favorite_customer#show'
-        get '/:id/revenue'  => 'revenue#show'
-        get '/find'         => 'find#show'
-        get '/find_all'     => 'find#index'
-        get '/random'       => 'random#show'
-        get '/:id/items'    => 'merchant_items#show'
-        get '/:id/invoices' => 'merchant_invoices#show'
+        get '/:id/revenue'           => 'revenue#show'
+        get '/find'                  => 'find#show'
+        get '/find_all'              => 'find#index'
+        get '/random'                => 'random#show'
+        get '/:id/items'             => 'merchant_items#show'
+        get '/:id/invoices'          => 'merchant_invoices#show'
       end 
       
       namespace :customers do 
-        get '/find'             => 'find#show'
-        get '/find_all'         => 'find#index'
-        get '/random'           => 'random#show'
-        get '/:id/invoices'     => 'customer_invoices#show'
-        get '/:id/transactions' => 'customer_transactions#show'
+        get '/:id/favorite_merchant' => 'favorite_merchant#show'
+        get '/find'                  => 'find#show'
+        get '/find_all'              => 'find#index'
+        get '/random'                => 'random#show'
+        get '/:id/invoices'          => 'customer_invoices#show'
+        get '/:id/transactions'      => 'customer_transactions#show'
       end 
       
       namespace :items do 
+        get '/most_revenue'      => 'most_revenue#index'
+        get '/most_items'        => 'most_items#index'
+        get '/:id/best_day'      => 'best_day#show'
         get '/find'              => 'find#show'
         get '/find_all'          => 'find#index'
         get '/random'            => 'random#show'
@@ -31,30 +35,30 @@ Rails.application.routes.draw do
       end 
       
       namespace :invoices do 
-        get '/find'             => 'find#show'
-        get '/find_all'         => 'find#index'
-        get '/random'           => 'random#show'
-        get '/random'           => 'random#show'
-        get '/:id/transactions' => 'invoice_transactions#show'
-        get '/:id/invoice_items'=> 'invoice_invoice_items#show'
-        get '/:id/items'        => 'invoice_items#show'
-        get '/:id/customer'     => 'invoice_customer#show'
-        get '/:id/merchant'     => 'invoice_merchant#show'
+        get '/find'              => 'find#show'
+        get '/find_all'          => 'find#index'
+        get '/random'            => 'random#show'
+        get '/random'            => 'random#show'
+        get '/:id/transactions'  => 'invoice_transactions#show'
+        get '/:id/invoice_items' => 'invoice_invoice_items#show'
+        get '/:id/items'         => 'invoice_items#show'
+        get '/:id/customer'      => 'invoice_customer#show'
+        get '/:id/merchant'      => 'invoice_merchant#show'
       end 
       
       namespace :invoice_items do 
-        get '/find'         => 'find#show'
-        get '/find_all'     => 'find#index'
-        get '/random'       => 'random#show'
-        get '/:id/item'     => 'invoice_item_item#show'
-        get '/:id/invoice'  => 'invoice_item_invoice#show'
+        get '/find'        => 'find#show'
+        get '/find_all'    => 'find#index'
+        get '/random'      => 'random#show'
+        get '/:id/item'    => 'invoice_item_item#show'
+        get '/:id/invoice' => 'invoice_item_invoice#show'
       end 
       
       namespace :transactions do 
-        get '/find'         => 'find#show'
-        get '/find_all'     => 'find#index'
-        get '/random'       => 'random#show'
-        get '/:id/invoice'  => 'transaction_invoice#show'
+        get '/find'        => 'find#show'
+        get '/find_all'    => 'find#index'
+        get '/random'      => 'random#show'
+        get '/:id/invoice' => 'transaction_invoice#show'
       end 
        
       resources :merchants, only: [:index, :show]
